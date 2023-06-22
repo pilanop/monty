4f06e2df2e13ce89c306898f5267eba8c767918f
Khalid Mohammed <khalarasta104@gmail.com>
2023-06-23 00:52:34 +0300

    nop

--- nop.c
+++ nop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * _nop - Does nothing.
+ * nop - Does nothing.
  * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_number: Interger representing the line number of of the opcode.
  */
-void _nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
