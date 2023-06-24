f77c93e192ba62990b8d308c3e649abf430fa0ed
Pauloslemma <pauloslemma1994@gmail.com>
2023-06-24 03:47:39 -0700

    pall and dup

--- dup.c
+++ dup.c
@@ -1,33 +1,33 @@
 #include "monty.h"
 
 /**
-* dup - duplicates the top element of the stack
-* @stack: double pointer to the top of the stack
-* @line_number: line number of the current opcode
-*
-* Return: void
-*/
-void dup(stack_t **stack, unsigned int line_number)
+ * dup - duplicates the top element of the stack and pushes it onto the stack.
+ * @stack: double pointer to the top of the stack
+ * @line: line number of the current opcode
+ *
+ * Return: void
+ */
+void dup(stack_t **stack, unsigned int line)
 {
-stack_t *new_node;
+	stack_t *new;
 
-if (*stack == NULL)
-{
-fprintf(stderr, "L%u: can't dup, stack empty\n", line_number);
-exit(EXIT_FAILURE);
-}
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't dup, stack empty\n", line);
+		exit(EXIT_FAILURE);
+	}
 
-new_node = malloc(sizeof(stack_t));
-if (new_node == NULL)
-{
-fprintf(stderr, "Error: malloc failed\n");
-exit(EXIT_FAILURE);
-}
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-new_node->n = (*stack)->n;
-new_node->prev = NULL;
-new_node->next = *stack;
-(*stack)->prev = new_node;
-*stack = new_node;
+	new->n = (*stack)->n;
+	new->prev = NULL;
+	new->next = *stack;
+	(*stack)->prev = new;
+	*stack = new;
 }
 
--- pall.c
+++ pall.c
@@ -3,21 +3,19 @@
 /**
 * pall - prints all the values on the stack, starting from the top of the stack
 * @stack: double pointer to the top of the stack
-* @line_number: line number of the current opcode
+* @line: line number of the current opcode
 *
 * Return: void
 */
-void pall(stack_t **stack, unsigned int line_number)
-{
-stack_t *tmp;
+void pall(stack_t **stack, unsigned int line) {
+	stack_t *temporary;
 
-(void)line_number;
+	(void) line;
 
-tmp = *stack;
-while (tmp != NULL)
-{
-printf("%d\n", tmp->n);
-tmp = tmp->next;
-}
+	temporary = *stack;
+	while (temporary != NULL) {
+		printf("%d\n", temporary->n);
+		temporary = temporary->next;
+	}
 }
 
