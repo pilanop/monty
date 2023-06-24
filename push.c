c3744b1377d2b47876607f056b926511ce606990
Pauloslemma <pauloslemma1994@gmail.com>
2023-06-24 03:21:23 -0700

    swap and push

--- push.c
+++ push.c
@@ -3,23 +3,21 @@
 /**
 * push - pushes an element to the stack
 * @stack: double pointer to the top of the stack
-* @line_number: line number of the current opcode
+* @line: line number of the current opcode
 *
 * Return: void
 */
-void push(stack_t **stack, unsigned int line_number)
-{
-char *arg = strtok(NULL, DELIMS);
-int n;
+void push(stack_t **stack, unsigned int line) {
+	char *arg = strtok(NULL, DELIMS);
+	int n;
 
-if (arg == NULL || _isdigit(arg) == 0)
-{
-fprintf(stderr, "L%u: usage: push integer\n", line_number);
-exit(EXIT_FAILURE);
-}
-n = atoi(arg);
+	if (arg == NULL || _isdigit(arg) == 0) {
+		fprintf(stderr, "L%u: usage: push integer\n", line);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
 
-add_node(stack, n);
+	add_node(stack, n);
 }
 
 /**
@@ -28,18 +26,16 @@ add_node(stack, n);
 *
 * Return: 1 if true, 0 if false
 */
-int _isdigit(char *str)
-{
-int i;
+int _isdigit(char *str) {
+	int i;
 
-for (i = 0; str[i]; i++)
-{
-if (str[i] == '-' && i == 0)
-continue;
-if (str[i] < '0' || str[i] > '9')
-return (0);
-}
-return (1);
+	for (i = 0; str[i]; i++) {
+		if (str[i] == '-' && i == 0)
+			continue;
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+	return (1);
 }
 
 /**
@@ -49,21 +45,20 @@ return (1);
 *
 * Return: void
 */
-void add_node(stack_t **head, const int n)
-{
-stack_t *new;
+void add_node(stack_t **head, const int n) {
+	stack_t *new;
 
-new = malloc(sizeof(stack_t));
-if (new == NULL)
-{
-fprintf(stderr, "Error: malloc failed\n");
-exit(EXIT_FAILURE);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL) {
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
 }
-new->n = n;
-new->prev = NULL;
-new->next = *head;
-if (*head != NULL)
-(*head)->prev = new;
-*head = new;
-}
+
 
--- swap.c
+++ swap.c
@@ -3,22 +3,20 @@
 /**
 * swap - swaps the top two elements of the stack
 * @stack: double pointer to the top of the stack
-* @line_number: line number of the current opcode
+* @line: line number of the current opcode
 *
 * Return: void
 */
-void swap(stack_t **stack, unsigned int line_number)
-{
-int tmp;
+void swap(stack_t **stack, unsigned int line) {
+	int temporary;
 
-if (*stack == NULL || (*stack)->next == NULL)
-{
-fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-exit(EXIT_FAILURE);
-}
+	if (*stack == NULL || (*stack)->next == NULL) {
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
 
-tmp = (*stack)->n;
-(*stack)->n = (*stack)->next->n;
-(*stack)->next->n = tmp;
+	temporary = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temporary;
 }
 
